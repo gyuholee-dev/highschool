26e86dbdd1c219bb801bc8378257894c0ed8223c
LeeGyuho <andrewgyuholee@gmail.com>
2022-06-01 20:44:41 +0900

    노티스 슬라이드 추가

--- scripts/script.js
+++ scripts/script.js
@@ -412,6 +412,11 @@
     const NewsSlide = new HS.NewsSlide(caster, 50, 0, 3500);
     NewsSlide.start();
 
+    // 노티스슬라이드
+    const notice = document.querySelector('#notice');
+    const NoticeSlide = new HS.NewsSlide(notice, 210, 0, 3500);
+    NoticeSlide.start();
+
   };
   const WindowLoaded = async()=>{
     // console.log("WINDOW LOADED");
--- scripts/src/main.js
+++ scripts/src/main.js
@@ -22,6 +22,11 @@ const DOMLoaded = async()=>{
   const NewsSlide = new HS.NewsSlide(caster, 50, 0, 3500);
   NewsSlide.start();
 
+  // 노티스슬라이드
+  const notice = document.querySelector('#notice');
+  const NoticeSlide = new HS.NewsSlide(notice, 210, 0, 3500);
+  NoticeSlide.start();
+
 }
 const WindowLoaded = async()=>{
   // console.log("WINDOW LOADED");
